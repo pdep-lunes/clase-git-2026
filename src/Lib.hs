9ca62bf1c1c9c59891682b30d986fb550286a110
Ornela Anahi Lugo Aquino <olugoaquino@frba.utn.edu.ar>
2026-05-04 09:31:05 -0300

    Rename function 'x' to 'suma' for clarity

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,6 @@ module Lib ( someFunc ) where
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-//Poco expresivo, no quiero esforzarme en entender
-x :: Int -> Int -> Int
-x a b = a + b
+
+suma :: Int -> Int -> Int
+suma unNumero otroNumero = unNumero + otroNumero
